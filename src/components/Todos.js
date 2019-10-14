f3830bfbccaa9227a11e0e96d1b2b8cc19e18851
Jon Tran <trungt8998@gmail.com>
2019-10-14 07:55:16 -0500

    display todo list on Todo compo

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -2,11 +2,9 @@ import React, { Component } from 'react';
 
 export class Todos extends Component {
   render() {
-    return (
-      <div>
-        <h1>App</h1>
-      </div>
-    );
+    return this.props.todos.map((todo) => (
+      <h3>{todo.title}</h3>
+    ));
   }
 }
 
