4d8b858c7c64af14d72f66d1e5875cab87024106
Jon Tran <trungt8998@gmail.com>
2019-10-14 07:51:54 -0500

    passing todos array to Todo component from App compo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
-import './App.css'
+import Todos from './components/Todos';
+import './App.css';
 
 export class App extends Component {
+
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'dinner with girlfriends',
+        completed: true
+      },
+      {
+        id: 3,
+        title: 'Meeting with colleague',
+        completed: false
+      }
+    ]
+  }
   render() {
+    console.log(this.state.todos);
     return (
       <div className="App">
-        <h1>App</h1>
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
