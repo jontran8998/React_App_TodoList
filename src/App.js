cf810d538a5c963adec9407fc172747546516550
Jon Tran <trungt8998@gmail.com>
2019-10-15 11:02:57 -0500

    solving the linethrough the task as checking

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export class App extends Component {
       {
         id: 2,
         title: 'dinner with girlfriends',
-        completed: true
+        completed: false
       },
       {
         id: 3,
@@ -23,11 +23,22 @@ export class App extends Component {
       }
     ]
   }
+
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo
+      })
+    })
+  }
+
   render() {
-    console.log(this.state.todos);
     return (
       <div className="App">
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
       </div>
     );
   }
--- src/components/TodoItems.js
+++ src/components/TodoItems.js
@@ -3,21 +3,22 @@ import PropTypes from 'prop-types'
 
 export default class TodoItems extends Component {
   getStyle = () => {
-    if (this.props.todo.completed) {
-      return {
-        textDecoration: 'line-through'
-      }
-    }
-    else {
-      return {
-        textDecoration: 'none'
-      }
+    return {
+      textDecoration: this.props.todo.completed ? 'line-through' : 'none',
+      padding: '10px',
+      borderBottom: '1px #ccc dotted',
+      background: '#f4f4f4'
     }
   }
+
   render() {
+    const { id, title } = this.props.todo
     return (
       <div style={this.getStyle()}>
-        <p>{this.props.todo.title}</p>
+        <p>
+          <input type="checkbox" onChange={this.props.markComplete.bind(this, id)} /> {' '}
+          {title}
+        </p>
       </div>
     )
   }
@@ -27,6 +28,3 @@ TodoItems.propTypes = {
   todo: PropTypes.object.isRequired
 }
 
-const itemStyle = {
-  backgroundColor: '#f4f4f4'
-}
