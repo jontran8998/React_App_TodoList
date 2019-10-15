19abb2d3958c89742e6433c724f58017e26372fc
Jon Tran <trungt8998@gmail.com>
2019-10-15 11:21:29 -0500

    add Header and HeadeerStyle

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Todos from './components/Todos';
 import './App.css';
+import Header from './components/layout/Header'
 
 export class App extends Component {
 
@@ -41,7 +42,9 @@ export class App extends Component {
 
   render() {
     return (
+
       <div className="App">
+        <Header />
         <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
       </div>
     );
